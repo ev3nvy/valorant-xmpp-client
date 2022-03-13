87c83fcb031e4358313d78c628f3ea288e18bd9e
Henrik Mertens <43936184+Henrik-3@users.noreply.github.com>
2022-03-13 17:31:01 +0100

    Add "region" element to "sender" and "recipient" object

--- src/helpers/parsers.ts
+++ src/helpers/parsers.ts
@@ -1,26 +1,37 @@
+import {XmppRegions} from './endpoints';
+
 // https://datatracker.ietf.org/doc/html/rfc7622#section-3.2
 export const parseJid = (jid: string): Jid => {
-    const [ restOfJid, ...resourcePart ] = jid.split('/');
+    const [restOfJid, ...resourcePart] = jid.split('/');
     const resource = resourcePart.join('/') || null;
 
-    const [ local, ...rest ] = restOfJid.split('@');
+    const [local, ...rest] = restOfJid.split('@');
 
     const domain = rest.join('@') || local;
-    return { local: rest.length === 0 ? null : local, domain, resource };
+
+    let region = null;
+    for(const xmppregion in XmppRegions) {
+        if(XmppRegions[xmppregion].domain === domain.split(".")[0]) {
+            region = XmppRegions[xmppregion].region;
+            break;
+        };
+    }
+    return {local: rest.length === 0 ? null : local, domain, resource, region};
 }
 
 // inefficient but makes the code look cleaner as I don't have to manually
 // parse every date element (arguments "need" to be string)
 export const parseLastOnline = (dateString: string): Date => {
-    const [ date, time ] = dateString.trim().split(/\ +/g);
-    const [ year, month, day ] = date.trim().split('-').map(element => parseInt(element));
-    const [ hour, min, sec, ms ] = time.trim().split('.').join(':').split(':').map(element => parseInt(element));
+    const [date, time] = dateString.trim().split(/\ +/g);
+    const [year, month, day] = date.trim().split('-').map(element => parseInt(element));
+    const [hour, min, sec, ms] = time.trim().split('.').join(':').split(':').map(element => parseInt(element));
 
-    return new Date(Date.UTC(year, month-1, day, hour, min, sec, ms));
+    return new Date(Date.UTC(year, month - 1, day, hour, min, sec, ms));
 }
 
 export interface Jid {
     local?: string
     domain: string
-    resource?: string
+    resource?: string,
+    region?: string
 }
