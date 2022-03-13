5f9456a6f9f97bc32a54f846a70bed2396cf805f
Henrik Mertens <43936184+Henrik-3@users.noreply.github.com>
2022-03-13 17:30:04 +0100

    Added "region" element

--- src/helpers/endpoints.ts
+++ src/helpers/endpoints.ts
@@ -3,30 +3,30 @@
 //          server resides so that I can give a non-generic name
 
 export class XmppRegions {
-    static Asia1 = { affinity: "jp1", domain: "jp1", lookupName: "asia" } // unsure
-    static Asia2 = { affinity: "as2", domain: "as2", lookupName: "as2" } // unsure
-    static Brasil = { affinity: "br", domain: "br1", lookupName: "br1" } // confident
-    static Europe1 = { affinity: "ru1", domain: "ru1", lookupName: "eu" } // unsure - possibly central eu
-    static Europe2 = { affinity: "eu3", domain: "eu3", lookupName: "eu3" } // unsure
-    static EuropeNordicAndEast = { affinity: "eun1", domain: "eu2", lookupName: "eun1" } // confident
-    static EuropeWest = { affinity: "euw1", domain: "eu1", lookupName: "euw1" } // confident
-    static Japan = { affinity: "jp1", domain: "jp1", lookupName: "jp1" } // confident
-    static LatinAmericaNorth = { affinity: "la1", domain: "la1", lookupName: "la1" } // confident
-    static LatinAmericaSouth = { affinity: "la2", domain: "la2", lookupName: "la2" } // confident
-    static NorthAmerica1 = { affinity: "na2", domain: "na1", lookupName: "na1" } // unsure
-    static NorthAmerica2 = { affinity: "la1", domain: "la1", lookupName: "us" } // unsure
-    static NorthAmerica3 = { affinity: "br", domain: "br1", lookupName: "us-br1" } // unsure
-    static NorthAmerica4 = { affinity: "la2", domain: "la2", lookupName: "us-la2" } // unsure
-    static NorthAmerica5 = { affinity: "us2", domain: "us2", lookupName: "us2" } // unsure
-    static Oceania = { affinity: "oc1", domain: "oc1", lookupName: "oc1" } // confident
-    static PBE = { affinity: "pbe1", domain: "pb1", lookupName: "pbe1" } // confident
-    static Russia = { affinity: "ru1", domain: "ru1", lookupName: "ru1" } // confident
-    static SouthEastAsia1 = { affinity: "sa1", domain: "sa1", lookupName: "sea1" } // unsure
-    static SouthEastAsia2 = { affinity: "sa2", domain: "sa2", lookupName: "sea2" } // unsure
-    static SouthEastAsia3 = { affinity: "sa3", domain: "sa3", lookupName: "sea3" } // unsure
-    static SouthEastAsia4 = { affinity: "sa4", domain: "sa4", lookupName: "sea4" } // unsure
-    static SouthKorea = { affinity: "kr1", domain: "kr1", lookupName: "kr1" } // confident
-    static Turkey = { affinity: "tr1", domain: "tr1", lookupName: "tr1" } // confident
+    static Asia1 = {affinity: "jp1", domain: "jp1", lookupName: "asia", region: "ap"} // unsure
+    static Asia2 = {affinity: "as2", domain: "as2", lookupName: "as2", region: "ap"} // unsure
+    static Brasil = {affinity: "br", domain: "br1", lookupName: "br1", region: "na"} // confident
+    static Europe1 = {affinity: "ru1", domain: "ru1", lookupName: "eu", region: "eu"} // unsure - possibly central eu
+    static Europe2 = {affinity: "eu3", domain: "eu3", lookupName: "eu3", region: "eu"} // unsure
+    static EuropeNordicAndEast = {affinity: "eun1", domain: "eu2", lookupName: "eun1", region: "eu"} // confident
+    static EuropeWest = {affinity: "euw1", domain: "eu1", lookupName: "euw1", region: "eu"} // confident
+    static Japan = {affinity: "jp1", domain: "jp1", lookupName: "jp1", region: "ap"} // confident
+    static LatinAmericaNorth = {affinity: "la1", domain: "la1", lookupName: "la1", region: "na"} // confident
+    static LatinAmericaSouth = {affinity: "la2", domain: "la2", lookupName: "la2", region: "na"} // confident
+    static NorthAmerica1 = {affinity: "na2", domain: "na1", lookupName: "na1", region: "na"} // unsure
+    static NorthAmerica2 = {affinity: "la1", domain: "la1", lookupName: "us", region: "na"} // unsure
+    static NorthAmerica3 = {affinity: "br", domain: "br1", lookupName: "us-br1", region: "na"} // unsure
+    static NorthAmerica4 = {affinity: "la2", domain: "la2", lookupName: "us-la2", region: "na"} // unsure
+    static NorthAmerica5 = {affinity: "us2", domain: "us2", lookupName: "us2", region: "na"} // unsure
+    static Oceania = {affinity: "oc1", domain: "oc1", lookupName: "oc1", region: "ap"} // confident
+    static PBE = {affinity: "pbe1", domain: "pb1", lookupName: "pbe1", region: "pbe"} // confident
+    static Russia = {affinity: "ru1", domain: "ru1", lookupName: "ru1", region: "eu"} // confident
+    static SouthEastAsia1 = {affinity: "sa1", domain: "sa1", lookupName: "sea1", region: "ap"} // unsure
+    static SouthEastAsia2 = {affinity: "sa2", domain: "sa2", lookupName: "sea2", region: "ap"} // unsure
+    static SouthEastAsia3 = {affinity: "sa3", domain: "sa3", lookupName: "sea3", region: "ap"} // unsure
+    static SouthEastAsia4 = {affinity: "sa4", domain: "sa4", lookupName: "sea4", region: "ap"} // unsure
+    static SouthKorea = {affinity: "kr1", domain: "kr1", lookupName: "kr1", region: "kr"} // confident
+    static Turkey = {affinity: "tr1", domain: "tr1", lookupName: "tr1", region: "eu"} // confident
 }
 
 export type Affinities = ("as2" | "jp1" | "br" | "ru1" | "eu3" | "eun1" | "euw1" | "kr1" | "la1" | "la2" | "na2" | "oc1" | "pbe1" | "sa1" | "sa2" | "sa3" | "sa4" | "tr1" | "us2") | (string & {});
